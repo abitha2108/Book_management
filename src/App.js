32db02f5513dec5ec4b4c5f2175e236ee588ff51
abitha2108 <abithamagi@gmail.com>
2026-01-04 13:47:50 +0530

    Production-ready build with dummy books

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [books] = useState([
+    { _id: 1, title: "Clean Code", author: "Robert C Martin" },
+    { _id: 2, title: "React in Action", author: "Mark Tielens" },
+    { _id: 3, title: "JavaScript: The Good Parts", author: "Douglas Crockford" },
+    { _id: 4, title: "You Don’t Know JS", author: "Kyle Simpson" },
+    { _id: 5, title: "Eloquent JavaScript", author: "Marijn Haverbeke" },
+    { _id: 6, title: "Introduction to Algorithms", author: "Cormen et al." },
+    { _id: 7, title: "Design Patterns", author: "Erich Gamma" }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "20px" }}>
+      <h1>Library Book Management</h1>
+      <ul>
+        {books.map(book => (
+          <li key={book._id}>
+            {book.title} - {book.author}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
